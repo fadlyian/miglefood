ce016c545e54c37cb8b1d35aefebfbf2411a833d
arifsptra <arif.sptrra@gmail.com>
2023-06-07 16:05:29 +0700

    fix display label and input phone number

--- resources/views/auth/login-customer.blade.php
+++ resources/views/auth/login-customer.blade.php
@@ -7,7 +7,7 @@
 
         <!-- PhoneNumber -->
         <div>
-            <x-input-label for="phoneNumber" :value="__('PhoneNumber')" />
+            <x-input-label for="phoneNumber" :value="__('Phone Number')" />
             <x-text-input id="phoneNumber" class="block mt-1 w-full" type="number" name="phoneNumber" :value="old('phoneNumber')" required autofocus autocomplete="phone-number" />
             <x-input-error :messages="$errors->get('phoneNumber')" class="mt-2" />
         </div>
@@ -24,5 +24,17 @@
             <span class="ml-1 text-[14px] text-gray-600">{{ __('Do not wish to receive any marketing or promotional materials') }}</span>
         </label>
     </form>
+    <style>
+        /* Chrome, Safari, Edge, Opera */
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        }
+
+        /* Firefox */
+        input[type=number] {
+        -moz-appearance: textfield;
+        }
+    </style>
     <script src="https://kit.fontawesome.com/1d72cf6431.js" crossorigin="anonymous"></script>
 </x-guest-layout>
