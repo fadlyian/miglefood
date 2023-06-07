0052e312b693d22f460bc0b95146860d7aa9723a
arifsptra <arif.sptrra@gmail.com>
2023-06-07 11:28:40 +0700

    improve login and component design

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-[#FFC529] border border-transparent rounded-full font-semibold text-xs text-black capitalize tracking-widest hover:bg-[#FFD669] focus:bg-[#FFD669] active:[#FFC529] focus:outline-none focus:ring-2 focus:ring-[#FFC529] focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'w-full text-center px-6 py-3 bg-[#FFC529] border border-transparent rounded-full font-semibold text-xs text-black capitalize tracking-widest hover:bg-[#FFD669] focus:bg-[#FFD669] active:[#FFC529] focus:outline-none focus:ring-2 focus:ring-[#FFC529] focus:ring-offset-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,14 +15,15 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center bg-gray-100">
+            <div class="w-full h-screen sm:h-full sm:max-w-md p-[20px] sm:px-[80px] sm:py-[60px] bg-white bg-[rgba(255, 255, 255, 0.8)] overflow-hidden sm:rounded-[20px]">
+                <div class="flex flex-col sm:justify-center items-center">
+                    <a href="/">
+                        <x-application-logo />
+                    </a>
+                    <p class="text-[24px] font-semibold mt-[20px]">Welcome!</p>
+                    <p class="text-[#727678] text-center mb-[30px] text-[14px]">Enter your Phone Number, and your Password and then click Log In button to continue to the application</p>
+                </div>
                 {{ $slot }}
             </div>
         </div>
