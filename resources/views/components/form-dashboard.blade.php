54bf7835d1ec70b46b588e202358e936cf17835a
arifsptra <arif.sptrra@gmail.com>
2023-06-07 21:13:58 +0700

    slicing form add account employee

---
+++ resources/views/components/form-dashboard.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    </head>
+    <body class="font-sans text-gray-900 antialiased">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center bg-gray-100">
+            <div class="w-full sm:max-w-md p-[20px] sm:px-[80px] sm:py-[60px] bg-white bg-[rgba(255, 255, 255, 0.8)] overflow-hidden sm:rounded-[20px]">
+                {{ $slot }}
+            </div>
+        </div>
+    </body>
+</html>
