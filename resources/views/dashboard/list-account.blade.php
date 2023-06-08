af1c6794773ad8046a7df1b1cf44178d9053e6a1
arifsptra <arif.sptrra@gmail.com>
2023-06-08 20:21:21 +0700

    slicing the account list view

--- resources/views/dashboard/list-account.blade.php
+++ resources/views/dashboard/list-account.blade.php
@@ -1,40 +1,40 @@
 <x-app-layout>
     @include('layouts.sidebar')
-    <div class="py-12 flex-auto">
+    <div class="ml-[45%] mt-10 py-12 flex-auto">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __('This is List Account!') }}
-
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Role</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            @foreach ($users as $user)
-                                <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $user->name }}</td>
-                                    <td>{{ $user->email }}</td>
-                                    <td>{{ $user->role }}</td>
-                                    <td>
-                                        <ul>
-
-                                        </ul>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+            @foreach ($users as $user)
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg my-3 max-w-[500px]">
+                <div class="p-6 text-gray-900 flex justify-between">
+                    <div>
+                        <p class="text-[24px] font-bold">{{ $user->name }}</p>
+                        <p class="capitalize">{{ $user->role }}</p>
+                        <p>{{ $user->email }}</p>
+                    </div>
+                    <div class="h-full mt-5">
+                        <button type="submit" class="text-[26px] mr-2 p-2 bg-green-600 border border-transparent rounded-lg font-semibold text-xs text-white capitalize tracking-widest hover:bg-green-500 active:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition ease-in-out duration-150" x-data="$user->id"><i class="fa-solid fa-arrows-rotate"></i></button>
+                        <button type="submit" class="text-[26px] p-2 bg-red-600 border border-transparent rounded-lg font-semibold text-xs text-white capitalize tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150" x-data="$user->id" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"><i class="fa-solid fa-trash"></i></button>
+                    </div>
                 </div>
             </div>
+            @endforeach
+            <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
+                <form method="post" action="" class="p-6">
+                    @csrf
+                    @method('delete')
+                    <h2 class="text-lg font-medium text-gray-900">
+                        {{ __('Are you sure you want to delete the account?') }}
+                    </h2>
+                    <div class="mt-6 flex justify-end">
+                        <x-secondary-button x-on:click="$dispatch('close')">
+                            {{ __('Cancel') }}
+                        </x-secondary-button>
+
+                        <x-danger-button class="ml-3">
+                            {{ __('Delete Account') }}
+                        </x-danger-button>
+                    </div>
+                </form>
+            </x-modal>
         </div>
     </div>
 </x-app-layout>
