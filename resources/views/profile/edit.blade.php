c5b07ff129707163467a016dc8716b33202a2d73
arifsptra <arif.sptrra@gmail.com>
2023-06-08 19:43:23 +0700

    fix display

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="py-12 mx-auto">
+    <div class="py-12 mx-auto mt-10">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-[20px] sm:px-[80px] sm:py-[60px] bg-white shadow sm:rounded-[20px]">
                 <div class="max-w-xl">
