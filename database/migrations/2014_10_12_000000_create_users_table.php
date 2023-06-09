5c192e84d2e6767cff07a8e38f3f079f121f6e9d
fadlyian <mansyuraw24@gmail.com>
2023-06-09 23:32:19 +0700

    delete field email_verified_at and rememberToke

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role');
-            $table->rememberToken();
+            // $table->rememberToken();
             $table->timestamps();
         });
     }
