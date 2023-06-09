b98995670fc1a9c21b8914bf050be9bc65982366
arifsptra <arif.sptrra@gmail.com>
2023-06-09 22:03:25 +0700

    slicing display list menu

--- resources/views/dashboard/add-menu.blade.php
+++ resources/views/dashboard/add-menu.blade.php
@@ -13,21 +13,21 @@
                 <!-- Name -->
                 <div>
                     <x-input-label for="name" :value="__('Name')" />
-                    <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="username" />
+                    <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required />
                     <x-input-error :messages="$errors->get('name')" class="mt-2" />
                 </div>
 
                 <!-- Category -->
                 <div class="mt-4">
                     <x-input-label for="category" :value="__('Category')" />
-                    <select name="category" id="category" class="w-full block mt-1 border-gray-300 focus:border-[#FFC529] focus:ring-[#FFC529] rounded-md shadow-sm" required autofocus>
+                    <select name="category" id="category" class="w-full block mt-1 border-gray-300 focus:border-[#FFC529] focus:ring-[#FFC529] rounded-md shadow-sm" required>
                         <option disabled selected>Choose One</option>
-                        <option value="cake">Cake</option>
-                        <option value="rice">Rice</option>
-                        <option value="meat">Meat</option>
-                        <option value="Fish">Fish</option>
-                        <option value="Milk">Milk</option>
-                        <option value="Drink">Drink</option>
+                        <option value="1">Cake</option>
+                        <option value="2">Rice</option>
+                        <option value="3">Meat</option>
+                        <option value="4">Fish</option>
+                        <option value="5">Milk</option>
+                        <option value="6">Drink</option>
                     </select>
                     <x-input-error :messages="$errors->get('role')" class="mt-2" />
                 </div>
@@ -35,25 +35,25 @@
                 <!-- Stock -->
                 <div class="mt-4">
                     <x-input-label for="stock" :value="__('Stock')" />
-                    <x-text-input id="stock" class="block mt-1 w-full" type="number" name="stock" :value="old('stock')" required autofocus autocomplete="stock" />
+                    <x-text-input id="stock" class="block mt-1 w-full" type="number" name="stock" :value="old('stock')" required/>
                     <x-input-error :messages="$errors->get('stock')" class="mt-2" />
                 </div>
 
                 <!-- Price -->
                 <div class="mt-4">
                     <x-input-label for="price" :value="__('Price')" />
-                    <x-text-input id="price" class="block mt-1 w-full" type="number" name="price" :value="old('price')" required autofocus autocomplete="price" />
+                    <x-text-input id="price" class="block mt-1 w-full" type="number" name="price" :value="old('price')" required/>
                     <x-input-error :messages="$errors->get('price')" class="mt-2" />
                 </div>
 
                 <!-- Choose Image File -->
                 <div class="mt-4">
                     <x-input-label for="image" :value="__('Choose Image')" class="mb-2" />
-                    <input type="file" id="actual-btn" name="image" :value="old('image')" required hidden/>
-                    <label class="mt-1 px-4 py-2 bg-white border border-gray-300 rounded-full font-semibold text-xs capitalize" for="actual-btn">Choose File</label>
+                    <input type="file" id="image" name="image" :value="old('image')" hidden/>
+                    <label class="mt-1 px-4 py-2 bg-white border border-gray-300 rounded-full font-semibold text-xs capitalize" for="image">Choose File</label>
                     <span class="text-[14px]" id="file-chosen">No file chosen</span>
                     <script>
-                        const actualBtn = document.getElementById('actual-btn');
+                        const actualBtn = document.getElementById('image');
                         const fileChosen = document.getElementById('file-chosen');
                         actualBtn.addEventListener('change', function(){
                         fileChosen.textContent = this.files[0].name
