cfeb1c08c75e6ecfdea3014c08a2d83c97b75ca7
arifsptra <arif.sptrra@gmail.com>
2023-06-09 21:20:04 +0700

    make function to send data product to route list-menu

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index(){
+        $data = Product::all();
+
+        return view('dashboard.list-menu',[
+            'products' => $data,
+        ]);
+    }
 }
