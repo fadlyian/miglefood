0578ed78bd1a917a948a6b214b5d60d804dae5d1
arifsptra <arif.sptrra@gmail.com>
2023-06-14 22:31:56 +0700

    fix tampilan list account

--- resources/views/dashboard/account/list-account.blade.php
+++ resources/views/dashboard/account/list-account.blade.php
@@ -5,17 +5,17 @@
             @foreach ($users as $user)
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg my-3 max-w-[500px]">
                 <div class="p-6 text-gray-900 flex justify-between">
-                    <div>
+                    <div class="w-[85%]">
                         <p class="text-[24px] font-bold">{{ $user->name }}</p>
                         <p class="capitalize">{{ $user->role }}</p>
                         <p>{{ $user->email }}</p>
                     </div>
-                    <div class="h-full mt-5">
-                        <form method="get" action="" class="p-6">
+                    <div class="w-[15%] mt-5 flex justify-between">
+                        <form method="get" action="">
                             @csrf
-                            <button type="submit" class="text-[26px] mr-2 p-2 bg-green-600 border border-transparent rounded-lg font-semibold text-xs text-white capitalize tracking-widest hover:bg-green-500 active:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition ease-in-out duration-150" x-data="$user->id"><i class="fa-solid fa-arrows-rotate"></i></button>
+                            <button type="submit" class="text-[26px] p-2 bg-green-600 border border-transparent rounded-lg font-semibold text-xs text-white capitalize tracking-widest hover:bg-green-500 active:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition ease-in-out duration-150" x-data="$user->id"><i class="fa-solid fa-arrows-rotate"></i></button>
                         </form>
-                        <form method="post" action="{{ route('delete-account', $user->id) }}" class="p-6">
+                        <form method="post" action="{{ route('delete-account', $user->id) }}">
                             @csrf
                             @method('delete')
                             <button type="submit" class="text-[26px] p-2 bg-red-600 border border-transparent rounded-lg font-semibold text-xs text-white capitalize tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150"><i class="fa-solid fa-trash"></i></button>
