437ca12fd4d2835d15c15ffde92a7f171d634826
arifsptra <arif.sptrra@gmail.com>
2023-06-14 22:39:10 +0700

    fix display list menu

--- resources/views/dashboard/menu/list-menu.blade.php
+++ resources/views/dashboard/menu/list-menu.blade.php
@@ -6,11 +6,11 @@
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg my-3 max-w-[700px]">
                     <div class="p-6 text-gray-900 flex justify-between">
                         @if ($product->image)
-                            <img src="{{ asset('storage/image/products/' . $product->image) }}" alt="{{ $product->image }}" class="h-[80px] w-[80px] flex-0 mr-6">
+                            {{-- <img src="{{ asset('storage/image/products/' . $product->image) }}" alt="{{ $product->image }}" class="h-[80px] w-[80px] flex-0 mr-6"> --}}
+                            <img src="{{ asset('/leker.png') }}" alt="Leker" class="max-h-[80px] max-w-[80px] flex-0 mr-6">
                         @else
-                            <img src="{{ asset('/leker.png') }}" alt="Leker" class="h-[80px] flex-0 mr-6">
                         @endif
-                        <div class="flex-1 mr-20">
+                        <div class="flex-1 mr-20 w-[60%]">
                             <p class="text-[24px] font-bold">{{ $product->name }}</p>
                             <div class="flex justify-between">
                                 <p>Remaining Stock</p>
@@ -21,14 +21,12 @@
                                 <p>{{ $product->price }}</p>
                             </div>
                         </div>
-                        <div class="h-full mt-5">
-                            <form method="get" action="{{ route('product.edit', $product->id) }}" class="p-6">
+                        <div class="w-[10%] flex justify-between items-center">
+                            <form method="get" action="{{ route('product.edit', $product->id) }}">
                                 <button type="submit"
-                                class="text-[26px] mr-2 p-2 bg-green-600 border border-transparent rounded-lg font-semibold text-xs text-white capitalize tracking-widest hover:bg-green-500 active:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition ease-in-out duration-150"><i class="fa-solid fa-arrows-rotate"></i></button>
+                                class="text-[26px] p-2 bg-green-600 border border-transparent rounded-lg font-semibold text-xs text-white capitalize tracking-widest hover:bg-green-500 active:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition ease-in-out duration-150"><i class="fa-solid fa-arrows-rotate"></i></button>
                             </form>
-                        </div>
-                        <div class="h-full mt-5">
-                            <form method="post" action="{{ route('product.destroy', $product->id) }}" class="p-6">
+                            <form method="post" action="{{ route('product.destroy', $product->id) }}">
                                 @csrf
                                 @method('delete')
                                 <button type="submit"
