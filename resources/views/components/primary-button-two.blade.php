9a652b407d3ac79472d4e6f1b4c5996f1223bad8
arifsptra <arif.sptrra@gmail.com>
2023-06-16 01:29:33 +0700

    make component primary button for customer view

---
+++ resources/views/components/primary-button-two.blade.php
@@ -0,0 +1,3 @@
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'w-full text-center py-1 bg-[#FFC529] border border-transparent rounded-full font-semibold text-xs text-black capitalize tracking-widest hover:bg-[#FFD669] focus:bg-[#FFD669] active:[#FFC529] focus:outline-none focus:ring-2 focus:ring-[#FFC529] focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+    {{ $slot }}
+</button>
