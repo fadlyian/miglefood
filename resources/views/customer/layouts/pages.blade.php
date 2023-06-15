4224e74ce7fe25c9cc3c7325720d5f9f223e7d35
arifsptra <arif.sptrra@gmail.com>
2023-06-16 01:39:47 +0700

    change font family to inter

--- resources/views/customer/layouts/pages.blade.php
+++ resources/views/customer/layouts/pages.blade.php
@@ -9,8 +9,9 @@
         <title>MigleFood</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@100&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -42,7 +43,7 @@
         <div class="fixed bottom-0 w-full m-auto">
             @include('customer.layouts.btmbar')
         </div>
-        
+
     </body>
     <script src="https://kit.fontawesome.com/1d72cf6431.js" crossorigin="anonymous"></script>
 </html>
