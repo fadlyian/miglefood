56fe0c6be24eb7f97bba6022af8255cff1ca92d5
arifsptra <arif.sptrra@gmail.com>
2023-06-17 11:40:04 +0700

    create cart view options

--- resources/views/customer/page/cart.blade.php
+++ resources/views/customer/page/cart.blade.php
@@ -1,4 +1,5 @@
 <x-pages-layout>
+@if(true)
     {{-- Main cart --}}
     @for($i = 0; $i < 20; $i++)
     <div class="flex flex-column mb-4 text-[14px]">
@@ -37,8 +38,13 @@
             <strong>IDR 356,400.00</strong>
         </x-format-text>
     </div>
-
     {{-- Button Confirm Order --}}
     <x-primary-button class="block shadow-[0px_0px_20px_-10px_rgba(0,0,0,0.2)] w-[95%] m-auto sm:max-w-md mb-20"> {{ __("Confirm Order") }}</x-primary-button>
+    </div>
+@else
+<div class="text-center shadow p-6 rounded-md text-[14px] hover:shadow-md transition ease-in-out duration-150 flex flex-col">
+    <strong>You don't have any products in your cart yet!</strong>
+    <a class="mt-4 text-center py-1 bg-[#FFC529] border border-transparent rounded-full font-semibold text-xs text-black capitalize tracking-widest hover:bg-[#FFD669] focus:bg-[#FFD669] active:[#FFC529] focus:outline-none focus:ring-2 focus:ring-[#FFC529] focus:ring-offset-2 transition ease-in-out duration-150" href="/home">Search for Products!</a>
 </div>
+@endif
 </x-pages-layout>
