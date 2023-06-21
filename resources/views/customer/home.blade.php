f0f31d69eb2d633af14284d6be2f4c22f98871f7
arifsptra <arif.sptrra@gmail.com>
2023-06-21 09:31:06 +0700

    create a table input view for the customer

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -1,12 +1,14 @@
 <x-pages-layout>
-    <div class="bg-blue-100 border border-blue-400 text-blue-700 px-4 py-3 rounded relative" role="alert">
+    <div id="input-table" class="bg-third border border-primary text-gray-900 px-4 py-3 rounded-lg relative mb-4" role="alert">
         <strong class="font-bold">Enter Table Number:</strong>
         <span class="block sm:inline">Please enter your table number below:</span>
         <form action="" method="POST" class="mt-3 sm:flex">
             {{-- {{ route('submit-table-number') }} --}}
             @csrf
-            <input type="text" name="table_number" class="w-full sm:w-auto px-2 py-1 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Nomor Meja" required>
-            <button type="submit" class="mt-2 sm:mt-0 sm:ml-2 px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">Submit</button>
+            <x-text-input id="table" class="block h-[30px] mr-4 w-[100%]" type="number" name="table" min="0" max="100" :value="old('table')" required />
+            <x-input-error :messages="$errors->get('table')" class="mt-2" />
+            {{-- <x-text-input type="text" name="table_number" class="w-full sm:w-auto px-2 py-1 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Nomor Meja" required> --}}
+            <x-primary-button-two type="submit">Submit</x-primary-button-two>
         </form>
     </div>
     <h2 class="text-[18px] font-bold">Recommended for you</h2>
@@ -55,19 +57,6 @@
                         </form>
                     </x-product-layout>
                     @endforeach
-                    {{-- @for($i = 0; $i < 10; $i++)
-                    <x-product-layout>
-                        <img src="{{ asset('/leker.png') }}" alt="" class="w-full">
-                        <div class="my-2">
-                            <strong class="text-[14px]">Leker Gosong</strong>
-                            <div class="flex justify-between text-[10px] my-1">
-                                <p class="">101 left</p>
-                                <i class="fa-solid fa-star text-[#FFC529]"> <span class="text-gray-900">4,8</span></i>
-                            </div>
-                            <strong class="text-[14px]">IDR 98,000.00</strong>
-                        </div>
-                    </x-product-layout>
-                    @endfor --}}
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,11 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                primary: '#FFC529',
+                secondary: '#FFE8AA',
+                third: '#FFF9E8',
+            },
         },
     },
 
