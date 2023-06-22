2071b0300126a08f6d2f40ce8c1f611ca59c4889
arifsptra <arif.sptrra@gmail.com>
2023-06-22 08:53:47 +0700

    make fitur edit account and logout for chef

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -33,14 +33,27 @@
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
 
+                            @if(Auth::user()->role == 'chef')
+                            <x-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')" class="p-1 ml-4">
+                                <i class="fa-solid fa-user text-[15px]"></i>
+                            </x-nav-link>
+                            <form method="POST" action="{{ route('logout') }}" class="inline-flex items-center">
+                                @csrf
+                                <x-nav-link href="route('logout')" onclick="event.preventDefault();
+                                            this.closest('form').submit();" class="w-full p-1 ml-4">
+                                    <i class="fa-solid fa-right-from-bracket text-[15px]"></i>
+                                </x-nav-link>
+                            </form>
                             {{-- <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
                             </div> --}}
+                            @endif
                         </button>
                     </x-slot>
-                    {{-- <x-slot name="content">
+                    {{-- @if(Auth::user()->role == 'chef')
+                    <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
@@ -55,7 +68,8 @@
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
-                    </x-slot> --}}
+                    </x-slot>
+                    @endif --}}
                 </x-dropdown>
             </div>
 
