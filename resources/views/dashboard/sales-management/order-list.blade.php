f890e6e2d2a1b8638ef51de8632a68f87eb612aa
arifsptra <arif.sptrra@gmail.com>
2023-06-22 09:46:13 +0700

    change view

--- resources/views/dashboard/sales-management/order-list.blade.php
+++ resources/views/dashboard/sales-management/order-list.blade.php
@@ -40,38 +40,42 @@
                     <div class="h-[2px] w-full bg-slate-100 my-3"></div>
                     <x-format-text>
                         <p>Sub Total</p>
-                        <strong>IDR {{ number_format($orders[$i]->subTotal, 2, ',', '.') }}</strong>
+                        <strong>IDR {{ number_format($ordersNotProcess[$i]->subTotal, 2, ',', '.') }}</strong>
                     </x-format-text>
                     <x-format-text>
                         <p>PPN (10%)</p>
-                        <strong>IDR {{ number_format($orders[$i]->ppn, 2, ',', '.') }}</strong>
+                        <strong>IDR {{ number_format($ordersNotProcess[$i]->ppn, 2, ',', '.') }}</strong>
                     </x-format-text>
                     <x-format-text>
                         <p>Grand Total</p>
-                        <strong>IDR {{ number_format($orders[$i]->grandTotal, 2, ',', '.') }}</strong>
+                        <strong>IDR {{ number_format($ordersNotProcess[$i]->grandTotal, 2, ',', '.') }}</strong>
                     </x-format-text>
                 </div>
             @endfor
+        @else
+        <div>
+            Kosong
+        </div>
         @endif
-
 
         <h2
             class="bg-yellow-500 max-w-max m-auto px-2 py-1 rounded-md mb-4 text-[24px] text-center font-bold text-white">
             Order On Process</h2>
-        @for ($i = 0; $i < count($orders); $i++)
+        @if (count($orderItemsProcess) > 0)
+        @for ($i = 0; $i < count($ordersProcess); $i++)
             {{-- @if ($orders[$i]->paymentStatus === 'on process') --}}
             <div class="bg-white text-[16px] p-6 shadow-sm hover:shadow-md rounded-lg w-full mb-6 transition ease-out">
                 <x-format-text>
                     <p>Customer Phone</p>
-                    <strong>{{ $orders[$i]->consumer->phoneNumber }}</strong>
+                    <strong>{{ $ordersProcess[$i]->consumer->phoneNumber }}</strong>
                 </x-format-text>
                 <x-format-text>
                     <p>Table Number</p>
-                    <strong>{{ $orders[$i]->tableNumber }}</strong>
+                    <strong>{{ $ordersProcess[$i]->tableNumber }}</strong>
                 </x-format-text>
                 <x-format-text>
                     <p>Order to</p>
-                    <strong>{{ $orders[$i]->id }}</strong>
+                    <strong>{{ $ordersProcess[$i]->id }}</strong>
                 </x-format-text>
                 <table class="w-full mt-6">
                     <tr>
@@ -79,7 +83,7 @@
                         <th class="text-left">Item</th>
                         <th class="text-right">Price</th>
                     </tr>
-                    @foreach ($orderItems[$i] as $orderItem)
+                    @foreach ($orderItemsProcess[$i] as $orderItem)
                         <tr>
                             <td>{{ $orderItem->quantity }}</td>
                             <td>{{ $orderItem->product->name }}</td>
@@ -90,24 +94,25 @@
                 <div class="h-[2px] w-full bg-slate-100 my-3"></div>
                 <x-format-text>
                     <p>Sub Total</p>
-                    <strong>IDR {{ number_format($orders[$i]->subTotal, 2, ',', '.') }}</strong>
+                    <strong>IDR {{ number_format($ordersProcess[$i]->subTotal, 2, ',', '.') }}</strong>
                 </x-format-text>
                 <x-format-text>
                     <p>PPN (10%)</p>
-                    <strong>IDR {{ number_format($orders[$i]->ppn, 2, ',', '.') }}</strong>
+                    <strong>IDR {{ number_format($ordersProcess[$i]->ppn, 2, ',', '.') }}</strong>
                 </x-format-text>
                 <x-format-text>
                     <p>Grand Total</p>
-                    <strong>IDR {{ number_format($orders[$i]->grandTotal, 2, ',', '.') }}</strong>
+                    <strong>IDR {{ number_format($ordersProcess[$i]->grandTotal, 2, ',', '.') }}</strong>
                 </x-format-text>
             </div>
             {{-- @endif --}}
-
         @endfor
+        @endif
 
         <h2
             class="bg-green-500 max-w-max m-auto mt-10 px-2 py-1 rounded-md mb-4 text-[24px] text-center font-bold text-white">
             Order Completed</h2>
+        @if (count($orderItemsDone) > 0)
         @for ($i = 0; $i < count($ordersDone); $i++)
             <div class="bg-white text-[16px] p-6 shadow-sm hover:shadow-md rounded-lg w-full mb-6 transition ease-out">
                 <x-format-text>
@@ -151,5 +156,6 @@
                 </x-format-text>
             </div>
         @endfor
+        @endif
     </div>
 </x-app-layout>
